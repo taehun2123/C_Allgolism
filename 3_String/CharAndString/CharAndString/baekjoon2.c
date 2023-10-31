8a6bb86d2d4470e025492baa6e1ec493c97d215e
taehun2123 <pyoneng_@naver.com>
2023-10-31 12:44:14 +0900

    백준 2번, 백준 3번 문자열 문제

---
+++ 3_String/CharAndString/CharAndString/baekjoon2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+  char str[100];
+
+  scanf("%s", str);
+
+  int length = strlen(str);
+
+  for(int i = 0; i<length; i++){
+    char temp = str[length-1];
+    for(int j=length-1; j>=0; j--){
+      str[j] = str[j-1];
+    }
+    str[0] = temp;
+    printf("%s\n", str);
+  }
+}
